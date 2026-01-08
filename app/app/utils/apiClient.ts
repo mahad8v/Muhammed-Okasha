28f63f73c73c97bbbdd82271f35381a445910db7
MahaD8v <mahabojang110@gmail.com>
2026-01-08 11:56:09 +0000

    added api client

---
+++ app/app/utils/apiClient.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://api.quran.com/api/v4',
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    console.log('API Error:', error?.response?.status, error?.message);
+
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
