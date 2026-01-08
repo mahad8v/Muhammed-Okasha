fb0dc66b128b16c24223d78d570bfcccee5db0d1
MahaD8v <mahabojang110@gmail.com>
2026-01-08 11:55:50 +0000

    added qibla screen

---
+++ app/app/(tabs)/qibla.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function Quibla() {
+  return (
+    <View>
+      <Text>Prayer Times</Text>
+    </View>
+  );
+}
