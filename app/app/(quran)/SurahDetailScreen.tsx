ae8743ff73a6dd0b26bdcced40dc5338ea41cf56
MahaD8v <mahabojang110@gmail.com>
2026-01-08 11:53:55 +0000

    Added surah details

---
+++ app/app/(quran)/SurahDetailScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function SurahDetailScreen() {
+  return (
+    <View>
+      <Text>Surah Detail</Text>
+    </View>
+  );
+}
