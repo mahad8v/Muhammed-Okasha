9c1784c7a0925ac293edc56882efd12402fc82bd
MahaD8v <mahabojang110@gmail.com>
2026-01-08 11:50:17 +0000

    setup the layout

---
+++ app/app/_layout.tsx
@@ -0,0 +1,38 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { queryClient } from './utils/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(fig)" options={{ headerShown: false }} />
+          <Stack.Screen name="(qibla)" options={{ headerShown: false }} />
+          <Stack.Screen name="(quran)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: 'modal', title: 'Modal' }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+}
