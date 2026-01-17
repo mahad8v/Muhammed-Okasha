3b4a05f2ee65a37edb3216601d8760000cbdfdb9
MahaD8v <mahabojang110@gmail.com>
2026-01-17 11:41:13 +0000

    added  card

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import React, { Children } from 'react';
+import { Colors } from '@/constants/theme';
 
-const card = () => {
+interface cardProps {
+  children: React.ReactNode;
+}
+
+const Card = ({ children }: cardProps) => {
+  const colorScheme = useColorScheme();
+  type ColorSchemeKey = keyof typeof Colors;
+  const scheme: ColorSchemeKey = (colorScheme ?? 'light') as ColorSchemeKey;
+  const colors = Colors[scheme];
+
   return (
-    <View>
-      <Text>card</Text>
+    <View style={[styles.card, { backgroundColor: colors.cardBg }]}>
+      {children}
     </View>
   );
 };
 
-export default card;
+export default Card;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 10,
+  },
+});
